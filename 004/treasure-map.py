4c8e123bc7d3e977f840215b6b553ae63247cd73
MichelleA <sonderbarmii@gmail.com>
2022-06-05 01:36:40 +0200

    edit code to simplify

--- 004/treasure-map.py
+++ 004/treasure-map.py
@@ -8,7 +8,6 @@ position = input("Where do you want to put the treasure? ")
 horizontal = int(position[0])
 vertical = int(position[1])
 
-row = map[vertical - 1]
-row[horizontal - 1] = "X"
+map[vertical - 1][horizontal - 1] = "X"
 
 print(f"{row1}\n{row2}\n{row3}")
