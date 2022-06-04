0bd8108e6bd5179025915b062019d9359cfe72fd
MichelleA <sonderbarmii@gmail.com>
2022-06-05 00:45:05 +0200

    edit code for better readability

--- 004/bankers-roulette.py
+++ 004/bankers-roulette.py
@@ -4,4 +4,5 @@ names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 
 random_name = random.randint(0, len(names)-1)
-print(f"{names[random_name]} is going to buy the meal today!")
+paying_person = names[random_name]
+print(paying_person + " is going to buy the meal today!")
