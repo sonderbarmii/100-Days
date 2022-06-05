544a9fac42181eb4c26dd50e8064c7775b0b2a63
MichelleA <sonderbarmii@gmail.com>
2022-06-05 03:00:01 +0200

    add day 4 rock-paper-scissors.py

---
+++ 004/rock-paper-scissors.py
@@ -0,0 +1,56 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+possible_choices = [rock, paper, scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. \n"))
+if user_choice >= 3 or user_choice < 0:
+  print("You typed an invalid number, you lose!")
+else:
+  print(f"You chose:")
+  print(str(possible_choices[user_choice]))
+  
+  computers_choice = random.randint(0, 2)
+  print(f"Computer chose:" )
+  print(str(possible_choices[computers_choice]))
+  
+  if user_choice == 0 and computers_choice == 2:
+    print("You win!")
+  elif computers_choice == 0 and user_choice == 2:
+    print("You lose.")
+  elif user_choice == computers_choice:
+    print("It's a draw.")
+  elif user_choice > computers_choice:
+    print("You win!")
+  elif user_choice < computers_choice:
+    print("You lose.")
+
+
+
+
+
