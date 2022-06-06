86fad3d6e0b8a8c8972b1e0acf19ed1bf0424a76
MichelleA <sonderbarmii@gmail.com>
2022-06-06 21:34:59 +0200

    add high-score.py and adding-even.py

---
+++ 005/adding-even.py
@@ -0,0 +1,5 @@
+total = 0
+for even_numbers in range(2, 101, 2):
+    total += even_numbers
+
+print(total)
--- 005/average-height.py
+++ 005/average-height.py
@@ -2,7 +2,7 @@ student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
 
-  total = 0
+total = 0
 for val in student_heights:
   total += val
 
