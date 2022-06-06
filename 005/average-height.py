11a762e8e0615c200f71d8e1dfa13940af20df15
MichelleA <sonderbarmii@gmail.com>
2022-06-06 20:58:35 +0200

    add average-height.py

---
+++ 005/average-height.py
@@ -0,0 +1,15 @@
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+
+  total = 0
+for val in student_heights:
+  total += val
+
+count = 0
+for element in student_heights:
+  count += 1
+
+avg_height = round(total / count)
+
+print(avg_height)
