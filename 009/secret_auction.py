b941a04269b8b705e7122c6059e47eda06affef4
MichelleA <sonderbarmii@gmail.com>
2022-06-10 01:26:08 +0200

    add secret_auction.py, code to "find highest bid"

--- 009/secret_auction.py
+++ 009/secret_auction.py
@@ -1,7 +1,31 @@
 from os import system
+from secret_auction_art import logo
+print(logo)
+
+end_of_auction = False
+list_of_bidders = {}
+
 def clear():
     _ = system("clear")
 
-print("Hello.")
+def add_new_bidder(add_name,  add_bid):
+  list_of_bidders[add_name] = add_bid
 
-clear()
+print("Welcome to the secret auction.")
+
+while end_of_auction:
+    name = input("What is your name? ")
+    bid = int(input("How much do you want to bid? "))
+    add_new_bidder(add_name=name, add_bid=bid)
+    more_bidders = input("Are there any other people who want to bid? Type 'yes' or 'no'. ").lower()
+    if more_bidders == "no:"
+        for i in list_of_bidders:
+            placed_bid = list_of_bidders[i]
+            
+    clear()    
+
+
+   
+
+print(list_of_bidders)  
+
