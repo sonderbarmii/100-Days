31bc044d6a90f66a16f85fd14f2866a35dd516bf
MichelleA <sonderbarmii@gmail.com>
2022-06-09 11:46:02 +0200

    add prime-number-checker.py

--- 008/Paint-Area-Calculator.py
+++ 008/Paint-Area-Calculator.py
@@ -1,11 +1,11 @@
 import math
-from turtle import width
+
+def paint_calc(height, width, cover):
+    num_of_cans = math.ceil((height * width) / cover)
+    print(f"You\'ll need {num_of_cans} cans of paint.")
+
+
 test_h = int(input("Height of wall: "))
 test_w = int(input("Width of wall: "))
 coverage = 5
-
-def paint_calc(height, width, cover):
-    num_of_cans = math.ceil((test_h * test_w) / coverage)
-    print(f"You'll need {num_of_cans} cans of paint.")
-
 paint_calc(height=test_h, width=test_w, cover=coverage)
