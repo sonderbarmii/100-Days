d39fd2440676a6b5cc00112bffadc5849b84c737
MichelleA <sonderbarmii@gmail.com>
2022-06-09 22:38:19 +0200

    finished caesar-cipher

--- 008/caesar-cipher.py
+++ 008/caesar-cipher.py
@@ -1,17 +1,32 @@
+from caesar_art import logo
+print(logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 def caesar(start_text, shift_amount, cipher_direction):
-    end_text = ""
-    if cipher_direction == "decode":
-        shift_amount *= -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
-    print(f"The {cipher_direction}d text is {end_text}")
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char not in alphabet:
+      end_text += char
+    else:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+  
+should_continue = True
 
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+while should_continue:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift = shift % 26
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  input_end = input("Do you want to restart the cipher program? Type 'yes' or 'no'. ").lower()
+  if input_end == 'no':
+    should_continue = False
+    print("Goodbye.")
