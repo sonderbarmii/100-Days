54d54a61623c41e9245025b7808096d5c3a40667
MichelleA <sonderbarmii@gmail.com>
2022-06-10 01:56:31 +0200

    finishing secret_auction.py

--- 009/secret_auction.py
+++ 009/secret_auction.py
@@ -1,31 +1,33 @@
 from os import system
 from secret_auction_art import logo
 print(logo)
-
 end_of_auction = False
 list_of_bidders = {}
+print("Welcome to the secret auction.")
 
 def clear():
     _ = system("clear")
 
-def add_new_bidder(add_name,  add_bid):
-  list_of_bidders[add_name] = add_bid
+def find_highest_bid(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of {highest_bid}.")
 
-print("Welcome to the secret auction.")
-
-while end_of_auction:
+while not end_of_auction:
     name = input("What is your name? ")
-    bid = int(input("How much do you want to bid? "))
-    add_new_bidder(add_name=name, add_bid=bid)
-    more_bidders = input("Are there any other people who want to bid? Type 'yes' or 'no'. ").lower()
-    if more_bidders == "no:"
-        for i in list_of_bidders:
-            placed_bid = list_of_bidders[i]
-            
-    clear()    
+    bid = int(input("How much do you want to bid? $"))
+    list_of_bidders[name] = bid
+    more_bidders = input("Are there any other bidders? Type 'yes' or 'no'. \n").lower()
+    if more_bidders == "no":
+        end_of_auction = True
+        find_highest_bid(list_of_bidders)
+    elif more_bidders == "yes":
+        clear()       
 
 
-   
-
-print(list_of_bidders)  
 
