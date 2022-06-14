62bb26ef4af19ca75a6746b60e60e985ca171980
MichelleA <sonderbarmii@gmail.com>
2022-06-14 22:06:50 +0200

    start code for oop coffee machine

--- 016/oop-coffee-machine-start/main.py
+++ 016/oop-coffee-machine-start/main.py
@@ -1,3 +1,23 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+import os
+
+clear = lambda: os.system('cls')
+
+making_coffee = True
+profit = 0
+
+# create objects from classes
+menu_item = MenuItem
+menu = Menu
+coffee_maker = CoffeeMaker
+money_machine = MoneyMachine
+
+while making_coffee:
+    start_process = input("What would you like to drink? (espresso/latte/cappuccino)\n").lower()
+    if start_process == "off":
+        making_coffee = False
+        clear
+    elif start_process == "report":
+        coffee_maker.report()
