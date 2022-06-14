911291ee0f4f50d1a2aabaea8585690504f24062
MichelleA <sonderbarmii@gmail.com>
2022-06-14 21:56:47 +0200

    add oop-coffee-machine zip to vscode

--- 016/Turtle_exercise.py
+++ 016/Turtle_exercise.py
@@ -1,11 +1,24 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-timmy = Turtle() # create new object from blueprint
-print(timmy)
-timmy.shape("turtle") # call methods that are associated with he object
-timmy.color('green') # call methods that are associated with he object
-timmy.forward(100) # call methods that are associated with he object
+# timmy = Turtle() # create new object from blueprint
+# print(timmy)
+# timmy.shape("turtle") # call methods that are associated with he object
+# timmy.color('green') # call methods that are associated with he object
+# timmy.forward(100) # call methods that are associated with he object
 
-my_screen = Screen()
-print(my_screen.canvheight) # tap into object's attributes
-my_screen.exitonclick()
+# my_screen = Screen()
+# print(my_screen.canvheight) # tap into object's attributes
+# my_screen.exitonclick()
+
+
+from prettytable import PrettyTable
+table = PrettyTable() # create new object from class
+
+# add data to columns
+table.add_column("Pokémon name",["Pikachu","Squirtle","Charmander"])
+table.add_column("Type",["Electric","Water","Fire"])
+print(table.align) # see alignment of table
+# change alignment 'r' 'l' and 'c'
+table.align = "l"
+
+print(table)
