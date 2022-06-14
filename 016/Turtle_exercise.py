319aca359ccf9147bff1303e4c357e8cf032138d
MichelleA <sonderbarmii@gmail.com>
2022-06-14 21:24:32 +0200

    add comments

--- 016/Turtle_exercise.py
+++ 016/Turtle_exercise.py
@@ -1,10 +1,11 @@
 from turtle import Turtle, Screen
 
-timmy = Turtle()
+timmy = Turtle() # create new object from blueprint
 print(timmy)
-timmy.shape("turtle")
-timmy.color('green')
+timmy.shape("turtle") # call methods that are associated with he object
+timmy.color('green') # call methods that are associated with he object
+timmy.forward(100) # call methods that are associated with he object
 
 my_screen = Screen()
-print(my_screen.canvheight)
+print(my_screen.canvheight) # tap into object's attributes
 my_screen.exitonclick()
