114aff220e3360d2fcacccec61f6530075dc4ac6
MichelleA <sonderbarmii@gmail.com>
2022-06-21 21:42:09 +0200

    add onkey and directions

--- 020/snake_game.py
+++ 020/snake_game.py
@@ -7,7 +7,10 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.listen()
-
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
 
 game_is_on = True
 screen.tracer(0)
