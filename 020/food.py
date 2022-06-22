e882c931713ad4755348c4e7bedda084678b3443
MichelleA <sonderbarmii@gmail.com>
2022-06-22 20:53:59 +0200

    make food refresh when collision with snake

--- 020/food.py
+++ 020/food.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import random
 
 class Food(Turtle):
 
@@ -9,3 +10,9 @@ class Food(Turtle):
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
--- 020/snake_game.py
+++ 020/snake_game.py
@@ -1,21 +1,22 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from snake import Snake
-
-
-snake = Snake()
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+
 screen.listen()
 screen.onkey(snake.up,"Up")
 screen.onkey(snake.down,"Down")
 screen.onkey(snake.left,"Left")
 screen.onkey(snake.right,"Right")
-screen.tracer(0)
-
 
 
 game_is_on = True
@@ -24,8 +25,11 @@ while game_is_on:
     time.sleep(0.1)
 
     # snake moves # tail follows head
-    snake.move_forward( )
+    snake.move_forward()
 
+    # detect collission with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
         
 
 
