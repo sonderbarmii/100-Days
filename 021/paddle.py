d6c0ff2f5b4a3c6b74204bd9ee688216a064b8a1
MichelleA <sonderbarmii@gmail.com>
2022-06-23 10:44:04 +0200

    create paddle and movements up and down

--- 021/paddle.py
+++ 021/paddle.py
@@ -1,15 +1,33 @@
 from turtle import Turtle
 
-class Paddle:
-.
-    def __init__(self):
-        self.segments = []
+STARTING_POSITION = (350,0)
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
 
+class Paddle:
+
+    def __init__(self):
+        self.create_paddle()
+        # x_pos = 350
+        # y_pos = 0
+        self.penup()
+        self.goto(STARTING_POSITION)
 
     def create_paddle(self):
-        for position in starting_positions:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+        self.paddle = self.shapesize(stretch_wid=20, stretch_len=100, outline=None)
+
+        # for position in starting_positions:
+        #     new_segment = Turtle("square")
+        #     new_segment.color("white")
+        #     new_segment.penup()
+        #     new_segment.goto(position)
+        #     self.segments.append(new_segment)
+
+    def up(self):
+        if self.paddle.heading() != DOWN:
+            self.paddle.setheading(UP)
+
+    def down(self):
+        if self.paddle.heading() != UP:
+            self.paddle.setheading(DOWN)
