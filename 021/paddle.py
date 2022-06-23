6b304626a8607b381e5e5e49b42556cf692d55c3
MichelleA <sonderbarmii@gmail.com>
2022-06-23 10:56:32 +0200

    edit to add paddle to main

--- 021/paddle.py
+++ 021/paddle.py
@@ -8,26 +8,14 @@ DOWN = 270
 class Paddle:
 
     def __init__(self):
-        self.create_paddle()
-        # x_pos = 350
-        # y_pos = 0
+        paddle = self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
         self.goto(STARTING_POSITION)
 
-    def create_paddle(self):
-        self.paddle = self.shapesize(stretch_wid=20, stretch_len=100, outline=None)
-
-        # for position in starting_positions:
-        #     new_segment = Turtle("square")
-        #     new_segment.color("white")
-        #     new_segment.penup()
-        #     new_segment.goto(position)
-        #     self.segments.append(new_segment)
-
     def up(self):
-        if self.paddle.heading() != DOWN:
-            self.paddle.setheading(UP)
+        new_y = self.ycor() + 20
+        self.goto(paddle.xcor(), new_y)
 
     def down(self):
-        if self.paddle.heading() != UP:
-            self.paddle.setheading(DOWN)
+        new_y = self.ycor() - 20
+        self.goto(paddle.xcor(), new_y)
--- 021/pong_game.py
+++ 021/pong_game.py
@@ -1,18 +1,23 @@
+from operator import truediv
 from turtle import Screen
+from paddle import Paddle
 
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+screen.tracer(0)
 
 p1 = Paddle()
 
 screen.listen()
-screen.onkey(paddle.up,"Up")
-screen.onkey(paddle.down,"Down")
+screen.onkeypress(p1.up,"Up")
+screen.onkeypress(p1.down,"Down")
 
-
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 
 
