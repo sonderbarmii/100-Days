806c173a52a31c1e652461ca8c485346b643384a
MichelleA <sonderbarmii@gmail.com>
2022-06-23 10:45:51 +0200

    import paddle to main

--- 021/pong_game.py
+++ 021/pong_game.py
@@ -6,7 +6,11 @@ screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
 
+p1 = Paddle()
 
+screen.listen()
+screen.onkey(paddle.up,"Up")
+screen.onkey(paddle.down,"Down")
 
 
 
