3d23f2bc43dff9b18c99b99ba4f3d5d17cd8e5ee
MichelleA <sonderbarmii@gmail.com>
2022-06-23 21:13:26 +0200

    set up starting code

---
+++ 023/Turtle_Crossing_Game/main.py
@@ -0,0 +1,16 @@
+from re import T
+from turtle import Screen
+import time
+import random
+
+
+screen = Screen()
+screen.tracer(0)
+screen.setup(width=600,height=600)
+screen.title("Turtle Crossing Game")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
