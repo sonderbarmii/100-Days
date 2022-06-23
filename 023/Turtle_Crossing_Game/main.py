3ceeb3f4669438f6342960dae758d247c0bd733d
MichelleA <sonderbarmii@gmail.com>
2022-06-23 22:26:54 +0200

    create cars and movement

--- 023/Turtle_Crossing_Game/main.py
+++ 023/Turtle_Crossing_Game/main.py
@@ -2,6 +2,7 @@ from operator import truediv
 from turtle import Screen
 import time
 from player import Player
+from car_manager import CarManager
 
 # screen setup
 screen = Screen()
@@ -10,6 +11,7 @@ screen.setup(width=600,height=600)
 screen.title("Turtle Crossing Game")
 
 tim = Player()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(tim.up,"Up")
@@ -20,7 +22,11 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
 
+    # create a new car, make it move
+    car_manager.create_cars()
+    car_manager.move_cars()
 
-
+    # detect turtle collision with car
+    
 
 screen.exitonclick()
