840bf5721cde0d16fe54cbba0823a3ff69ae8909
MichelleA <sonderbarmii@gmail.com>
2022-06-30 11:06:27 +0200

    play around with txt files

--- 024/exercise/main.py
+++ 024/exercise/main.py
@@ -1,2 +1,8 @@
-file = open("024/exercise/my_file.txt") 
-file.read()
+# reading a file
+# with open("024/exercise/my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# write to a file
+with open("024/exercise/new_file.txt", mode="a") as file:
+    file.write("\nNew text, ")
