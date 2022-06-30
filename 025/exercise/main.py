62eb8a199fc497f5bfc4d4a549f6296bc67d3dcd
MichelleA <sonderbarmii@gmail.com>
2022-06-30 15:27:29 +0200

    create new file

---
+++ 025/exercise/main.py
@@ -0,0 +1,15 @@
+# with open("025/exercise/weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+# import csv
+
+# with open("025/exercise/weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+
+import pandas
