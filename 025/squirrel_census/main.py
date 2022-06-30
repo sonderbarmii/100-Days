311061cea6b97e4f876991c387416c3b2a4f9da0
MichelleA <sonderbarmii@gmail.com>
2022-06-30 23:03:18 +0200

    finished exercise successfully

--- 025/squirrel_census/main.py
+++ 025/squirrel_census/main.py
@@ -2,20 +2,27 @@ import pandas
 
 data = pandas.read_csv("025/squirrel_census/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 
-# Find out how many gray, cinnamon or black squirrels? (from Primary Fur Color)
-#fur_color = data["Primary Fur Color"]
+# My Solution
 gray_squirrels = sum(data["Primary Fur Color"] == "Gray")
-#print(sum(gray_squirrels))
-
 cin_squirrels = sum(data["Primary Fur Color"] == "Cinnamon")
-#print(sum(cin_squirrels))
-
 black_squirrels = sum(data["Primary Fur Color"] == "Black")
-#print(sum(black_squirrels))
 
 data_dict = {
-    "Color": ["Gray", "Cinnamon", "Black"],
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
     "Count": [gray_squirrels, cin_squirrels, black_squirrels],
 }
 new_data = pandas.DataFrame(data_dict)
 new_data.to_csv("025/squirrel_census/color_count.csv")
+
+# Angelas Solution
+# data = pandas.read_csv("025/squirrel_census/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+# gray_squirrels = len(data[(data["Primary Fur Color"] == "Gray")])
+# cin_squirrels = len(data[(data["Primary Fur Color"] == "Cinnamon")])
+# black_squirrels = len(data[(data["Primary Fur Color"] == "Black")])
+
+# data_dict = {
+#     "Fur Color": ["Gray", "Cinnamon", "Black"],
+#     "Count": [gray_squirrels, cin_squirrels, black_squirrels],
+# }
+# new_data = pandas.DataFrame(data_dict)
+# new_data.to_csv("025/squirrel_census/color_count.csv")
