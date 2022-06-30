13915f9d7a0dff878e512275fcb2c0538100ca90
MichelleA <sonderbarmii@gmail.com>
2022-06-30 22:55:11 +0200

    attempt own solution

--- 025/squirrel_census/main.py
+++ 025/squirrel_census/main.py
@@ -4,12 +4,18 @@ data = pandas.read_csv("025/squirrel_census/2018_Central_Park_Squirrel_Census_-_
 
 # Find out how many gray, cinnamon or black squirrels? (from Primary Fur Color)
 #fur_color = data["Primary Fur Color"]
-gray_squirrels = (data["Primary Fur Color"] == "Gray")
+gray_squirrels = sum(data["Primary Fur Color"] == "Gray")
 #print(sum(gray_squirrels))
 
-cin_squirrels = (data["Primary Fur Color"] == "Cinnamon")
+cin_squirrels = sum(data["Primary Fur Color"] == "Cinnamon")
 #print(sum(cin_squirrels))
 
-black_squirrels = (data["Primary Fur Color"] == "Black")
+black_squirrels = sum(data["Primary Fur Color"] == "Black")
 #print(sum(black_squirrels))
 
+data_dict = {
+    "Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels, cin_squirrels, black_squirrels],
+}
+new_data = pandas.DataFrame(data_dict)
+new_data.to_csv("025/squirrel_census/color_count.csv")
