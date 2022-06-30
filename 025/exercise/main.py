e46136402c1266ec7130c217d34e40c9e6831cf3
MichelleA <sonderbarmii@gmail.com>
2022-06-30 22:31:30 +0200

    go through lesson challenges

--- 025/exercise/main.py
+++ 025/exercise/main.py
@@ -12,4 +12,36 @@
 #             temperatures.append(int(row[1]))
 #     print(temperatures)
 
+from audioop import avg
 import pandas
+
+# data = pandas.read_csv("025/exercise/weather_data.csv")
+# print(data["temp"])
+# data_dict = data.to_dict()
+# print(data_dict)
+
+# temp_list = data["temp"].to_list()
+# find average
+# print(round(sum(temp_list)/len(temp_list),2)) 
+# print(data["temp"].mean())
+# find maximum number
+# print(data["temp"].max())
+
+# get data from row
+# print(data[data.day == "Monday"])
+# get the row of data which had the highest temperature
+# print(data[data.temp == data.temp.max()])
+
+# access values from a row
+# monday = data[data.day == "Monday"]
+# convert celsius to fahrenheit
+# monday_temp = int((monday.temp*9/5)+32)
+# print(monday_temp)
+
+# create dataframe from scratch
+# data_dict = {
+#     "students" : ["Amy", "James", "Angela"],
+#     "scores" : [76,56,65]
+# }
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("025/exercise/new_data.csv") # create new file
